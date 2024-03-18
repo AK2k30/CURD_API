8590f089c33506946e7cebc78b0b470931e6c513
Akash Singh <akashsunilsingh5555@gmail.com>
2024-03-18 23:27:10 +0530

    product update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.get('/about', (req, res) => {
-    res.send("hii")
+// routes
+app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
 });
 
 mongoose.connect("mongodb+srv://akashsunilsingh5555:admin@apibackend.71vgjff.mongodb.net/Node-API?retryWrites=true&w=majority&appName=APIBackend").then(() => {
--- models/product.model.js
+++ models/product.model.js
@@ -11,6 +11,23 @@ const ProductSchema = mongoose.Schema(
             type: Number,
             required: true,
             default: 0
-        }
+        },
+
+        price: {
+            type: Number,   
+            required: true,
+            default: 0
+        },
+
+        image: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
     }
-)
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
